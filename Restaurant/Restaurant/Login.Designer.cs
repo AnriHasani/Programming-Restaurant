774e23a8918e1af137f24918ca315f2e2ca8d9dd
AnriHasani <anrihasani9@gmail.com>
2025-05-24 10:02:14 +0200

    Add files via upload

---
+++ Restaurant/Restaurant/Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Restaurant
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_Reservation = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // btn_Reservation
+            // 
+            btn_Reservation.Font = new Font("Segoe UI", 14F);
+            btn_Reservation.Location = new Point(263, 186);
+            btn_Reservation.Name = "btn_Reservation";
+            btn_Reservation.Size = new Size(257, 54);
+            btn_Reservation.TabIndex = 0;
+            btn_Reservation.Text = "Make a reservation";
+            btn_Reservation.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F);
+            label1.Location = new Point(204, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(408, 54);
+            label1.TabIndex = 1;
+            label1.Text = "Restaurant Intial Page";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(332, 277);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Admin Login";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(331, 322);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.ImeMode = ImeMode.On;
+            textBox2.Location = new Point(331, 355);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(344, 388);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 453);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btn_Reservation);
+            Name = "Login";
+            Text = "Form1";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_Reservation;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
